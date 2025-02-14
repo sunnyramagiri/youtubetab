4364318bac0bf22222d027d8ce177eb708751699
sunnyramagiri <ramagirisunny.uk@gmail.com>
2025-02-14 22:42:45 +0530

    push

---
+++ project.groovy
@@ -0,0 +1,34 @@
+pipeline{
+    agent any
+    environment {
+        DOCKERHUB_CREDENTIALS = credentials ('fsunnyramagiri')
+    }
+    stages {
+        stage('SCM checkout') {
+            steps{
+               git 'https://github.com/sunnyramagiri/youtubetab.git'
+            }
+        }
+    }
+    stage('Build docker image') {
+        steps {
+            sh 'docker build -t name of the sunnramagiri/nodeapp:$BUILD_NUMBER .'
+        }
+    }
+    stage ('login to dockerhub') {
+        steps{
+            sh 'echo $DOCKERHUB_CREDENTIALS_PWS | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+        }
+    }
+    stage('push image') {
+        stages{
+            sh 'docker push sunnyramagiri/nodeapp:$BUILD_NUMBER'
+        }
+    }
+}
+post {
+       always {
+           sh 'docker logout'}
+        }
+    }
+}
